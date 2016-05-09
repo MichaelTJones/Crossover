5900c85a845f3abd5d1b44e78c6cb7200c8a09df
Michael T Jones <michael.jones@gmail.com>
2016-05-09 18:34:52 +0900

    Use processor count to adjust dynamic threshold

--- sum.go
+++ sum.go
@@ -2,19 +2,22 @@ package sum
 
 import "runtime"
 
-func SumInts(ints []int) int {
-	if len(ints) < 28728 { // estimated by TestCrossover using "go test -v"
-		return SumIntsSerial(ints)
+func SumInts(ints []int) (sum int) {
+	parallel := min(runtime.NumCPU(), runtime.GOMAXPROCS(0)) // degree of parallelism
+	// estimated by "go test -v -run=TestCrossover" with various GOMAXPROCS settings
+	if parallel == 1 || (parallel == 2 && len(ints) < 44000) || len(ints) < 22000 {
+		sum = SumIntsSerial(ints)
+	} else {
+		sum = SumIntsParallel(ints)
 	}
-	return SumIntsParallel(ints)
+	return
 }
 
-func SumIntsSerial(ints []int) int {
-	sum := 0
+func SumIntsSerial(ints []int) (sum int) {
 	for _, v := range ints {
 		sum += v
 	}
-	return sum
+	return
 }
 
 func min(a, b int) int {
@@ -24,10 +27,10 @@ func min(a, b int) int {
 	return b
 }
 
-func SumIntsParallel(ints []int) int {
-	ncpus := runtime.NumCPU() // degree of parallelism
-	result := make(chan int, ncpus)
-	chunk := (len(ints) + ncpus - 1) / ncpus
+func SumIntsParallel(ints []int) (sum int) {
+	parallel := min(runtime.NumCPU(), runtime.GOMAXPROCS(0)) // degree of parallelism
+	result := make(chan int, parallel)
+	chunk := (len(ints) + parallel - 1) / parallel
 	go func() {
 		for start := 0; start < len(ints); start += chunk {
 			go func(k []int) {
@@ -36,11 +39,9 @@ func SumIntsParallel(ints []int) int {
 		}
 	}()
 
-	sum := 0
 	for start := 0; start < len(ints); start += chunk {
 		sum += <-result
 	}
 	close(result)
-
-	return sum
+	return
 }
